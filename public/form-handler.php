654a594d3ae8c20ca1494402f6300e94da342950
Startup Café <startupcafe226@gmail.com>
2023-03-23 12:25:58 +0300

    added succes.html and form validation

--- public/form-handler.php
+++ public/form-handler.php
@@ -1,20 +1,39 @@
 <?php
+
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $name = $_POST["name"];
-    $email = $_POST["email"];
-    $stakeholder = $_POST["stakeholder"];
-    
-    $to = "info@ease.et";
-    $subject = "New Subscription from EASE Coming Soon Page";
-    $message = "Name: " . $name . "\n" . "Email: " . $email . "\n" . "Stakeholder: " . $stakeholder;
-    $headers = "From: no-reply@ease.et\r\n";
+    $name = trim($_POST["name"]);
+    $email = trim($_POST["email"]);
+    $stakeholder = trim($_POST["stakeholder"]);
+
+    // Perform validation
+    if (empty($name) || empty($email) || empty($stakeholder)) {
+        die("Error: All fields are required.");
+    }
+
+    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        die("Error: Invalid email address.");
+    }
+
+    // Sanitize input
+    $name = filter_var($name, FILTER_SANITIZE_STRING);
+    $email = filter_var($email, FILTER_SANITIZE_EMAIL);
+    $stakeholder = filter_var($stakeholder, FILTER_SANITIZE_STRING);
+
+    // Process the form data (e.g. send an email)
+    $to = "cees.wanrooij@ease.et";
+    $subject = "New subscriber from EASE coming soon page";
+    $message = "Name: {$name}\nEmail: {$email}\nStakeholder: {$stakeholder}";
+    $headers = "From: no-reply@ease.et";
 
     if (mail($to, $subject, $message, $headers)) {
-        header("Location: thank-you.html");
+        header("Location: success.html");
+        exit;
     } else {
-        header("Location: error.html");
+        die("Error: Failed to send email.");
     }
 } else {
     header("Location: index.html");
+    exit;
 }
+
 ?>
